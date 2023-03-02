42dc52d678f57a6b3a4a7474a299dc396ec4f5f6
kevin_avot <kevin_avot@univ-artois.fr>
2023-03-02 15:46:00 +0100

    modified light switch with a list of spotlight and experimenting selection of cured objects

--- Source/Exorcism/Public/CursableObject.h
+++ Source/Exorcism/Public/CursableObject.h
@@ -11,11 +11,18 @@ class EXORCISM_API ACursableObject : public AActor
 {
 	GENERATED_BODY()
 
-	bool IsCursed;
+	UPROPERTY(BlueprintGetter = GetIsCursed, BlueprintSetter = SetIsCursed)
+	bool IsCursed = false;
 	
 public:	
 	// Sets default values for this actor's properties
 	ACursableObject();
+
+	UFUNCTION(BlueprintPure, BlueprintInternalUseOnly)
+		bool GetIsCursed() const { return IsCursed; }
+
+	UFUNCTION(BlueprintCallable, BlueprintInternalUseOnly)
+		void SetIsCursed(const bool cursed) { IsCursed = cursed; }
 
 protected:
 	// Called when the game starts or when spawned
